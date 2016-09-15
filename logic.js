8a36dc0e609f988009de8e2d2fb924bd975ce3d6
Matthew Lin <Matthew Lin>
2016-09-15 16:09:34 -0400

    Updated JS file. Successfully linked api with project and can access GIFs to display on page with search term buttons

--- logic.js
+++ logic.js
@@ -2,104 +2,73 @@
 
 	// ========================================================
 
-	// displayMovieInfo function now re-renders the HTML to display the appropriate content. 
-	function displayHeroInfo(){
+	// function displayHeroInfo(){
 
-		var hero = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q" + hero + "&q=&rating=&limit=";
+	// 	var heroes = $(this).data("name");
+	// 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + heroes + "&api_key=dc6zaTOxFJmzC&limit=10";
 		
-		// Creates AJAX call for the specific movie being 
+
+	function displayHeroInfo(){
+$("#heroesView").empty();
+var type = ($(this).data("name"))
+var queryURL = "https://api.giphy.com/v1/gifs/search?q="+type+"&limit=10&api_key=dc6zaTOxFJmzC";
 		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 
-			// Creates a generic div to hold the movie
+			console.log(response);
+
 			var heroDiv = $('<div class="hero">');
+			var heroImage = $('<div class="heroImage">');
 
-			// Retrieves the Rating Data
-			var rating = response.Rated;
+			var results = response.data;
+			for (var i = 0; i < results.length; i++){
+         		var heroDiv = $('<div class="hero">');
 
-			// Creates an element to have the rating displayed
-			var pOne = $('<p>').text( "Rating: " + rating);
+         	var rating = results[i].rating;
 
-			// Displays the rrating
-			movieDiv.append(pOne);
+         	var pOne = $('<p>').text( "Rating: " + rating);
+         	heroDiv.append(pOne);
+         	};
 
-			// Retrieves the release year
-			var released = response.Released;
+			var results = response.data;
+			for (var i = 0; i < results.length; i++){
+				var heroImage = $('<div class="heroImage">');
+         		var image = results[i].images.fixed_height.url;
 
-			// Creates an element to hold the release year
-			var pTwo = $('<p>').text( "Released: " + released);
-
-			// Displays the release year
-			movieDiv.append(pTwo);
-
-			// Retrieves the plot
-			var plot = response.Plot;
-
-			// Creates an element to hold the plot
-			var pThree = $('<p>').text( "Plot: " + plot);
-
-			// Appends the plot
-			movieDiv.append(pThree);
-
-			// Creates an element to hold the image 
-			var image = $('<img>').attr("src", response.Poster);
-
-			// Appends the image
-			movieDiv.append(image);
-
-			// Puts the entire Movie above the previous movies.
-			$('#moviesView').prepend(movieDiv);
+         		var pTwo = $('<img>').attr("src", response.data[i].images.fixed_height.url);
+         		heroDiv.append(pTwo);
+         };
+			
+			$('#heroesView').prepend(heroDiv);
 		});
 
 	}
 
-	// ========================================================
-
-	// Generic function for displaying movie data 
 	function renderButtons(){ 
-
-		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
 		$('#buttonsView').empty();
 
-		// Loops through the array of movies
 		for (var i = 0; i < heroes.length; i++){
-
-			// Then dynamicaly generates buttons for each movie in the array
-
-			// Note the jQUery syntax here... 
-		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('hero'); // Added a class 
-		    a.attr('data-name', heroes[i]); // Added a data-attribute
-		    a.text(heroes[i]); // Provided the initial button text
-		    $('#buttonsView').append(a); // Added the button to the HTML
+		    var a = $('<button>')
+		    a.addClass('hero');
+		    a.attr('data-name', heroes[i]);
+		    a.text(heroes[i]);
+		    $('#buttonsView').append(a);
 		}
 	}
 
 	// ========================================================
 
-	// This function handles events where one button is clicked
 	$('#addHero').on('click', function(){
 
-		// This line of code will grab the input from the textbox
 		var hero = $('#hero-input').val().trim();
-
-		// The movie from the textbox is then added to our array
 		heroes.push(hero);
-		
-		// Our array then runs which handles the processing of our movie array
 		renderButtons();
-
-		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
 		return false;
 	})
 
 	// ========================================================
-
-	// Generic function for displaying the movieInfo
 	$(document).on('click', '.hero', displayHeroInfo);
 
 
 	// ========================================================
 
-	// This calls the renderButtons() function
 	renderButtons();
